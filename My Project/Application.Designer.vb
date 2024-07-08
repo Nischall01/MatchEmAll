41812af9f3bccc0e160530f1e377f50909c3c833
Nischal <thapanischal39@gmail.com>
2024-07-08 23:50:34 +0545

    #Name_Change

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Game.Entry
+            Me.MainForm = Global.MatchEmAll.Entry
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -158,9 +158,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Game.My.MySettings
+        Friend ReadOnly Property Settings() As Global.MatchEmAll.My.MySettings
             Get
-                Return Global.Game.My.MySettings.Default
+                Return Global.MatchEmAll.My.MySettings.Default
             End Get
         End Property
     End Module
